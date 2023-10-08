23b163ddf2aee25d9e952ec9527900561d338d34
cristianaquino <ycristian10@gmail.com>
2023-10-08 00:37:08 -0500

    feat: routes of project

---
+++ src/routes/route.ts
@@ -0,0 +1,14 @@
+export const PRIVATE_ROUTE = {};
+export const PUBLIC_ROUTE = {
+  HOME: "/",
+  PROYECTS: "/proyects",
+  COMPONENTS: "/components",
+};
+export const SERVICE_ROUTE = {
+  ALLPRODUCTS: "/products",
+  CHECKOUT: "/checkout",
+  ADDPRODUCT: "/addproduct",
+};
+export const PROYECTS_ROUTE = {
+  ECOMMERS: "/ecommers",
+};
