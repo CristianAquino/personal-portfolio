534913c92b824287ff41e84d3710624a13ef0aef
cristianaquino <ycristian10@gmail.com>
2023-10-08 00:39:25 -0500

    feat: create routes of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route } from "react-router-dom";
+import { RoutesWithNotFound } from "./helpers";
+import { PUBLIC_ROUTE } from "./routes";
+import "./App.css";
+import { Components, Proyects } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <RoutesWithNotFound pageRedirect={PUBLIC_ROUTE.HOME}>
+        <Route path={PUBLIC_ROUTE.HOME} element={<div>Home</div>} />
+        {/* anadimos el * para que cualquier ruta acceda mediante proyects/[page_name] */}
+        <Route path={`${PUBLIC_ROUTE.PROYECTS}/*`} element={<Proyects />} />
+        <Route path={`${PUBLIC_ROUTE.COMPONENTS}/*`} element={<Components />} />
+      </RoutesWithNotFound>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
