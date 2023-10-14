ac4d68bb447fcc9a8e12875d513288759f1c36ff
cristianaquino <ycristian10@gmail.com>
2023-10-13 23:21:30 -0500

    feat: barrel

--- src/pages/Proyects/Ecommerce/components/index.ts
+++ src/pages/Proyects/Ecommerce/components/index.ts
@@ -1,3 +1,7 @@
 export * from "./Button";
 export * from "./Navigator";
+export * from "./NavigatorProducts";
 export * from "./Panel";
+export * from "./CardProduct";
+export * from "./Products";
+export * from "./GridProducts";
