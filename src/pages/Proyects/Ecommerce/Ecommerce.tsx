d8a108eb608b11ff0ce9c1aecb58cc722a7a1604
cristianaquino <ycristian10@gmail.com>
2023-10-14 20:12:58 -0500

    feat: add provider

--- src/pages/Proyects/Ecommerce/Ecommerce.tsx
+++ src/pages/Proyects/Ecommerce/Ecommerce.tsx
@@ -1,17 +1,26 @@
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { Navigator, Panel, Products } from "./components";
+import { CartProvider } from "./context/CartContext";
+const Cart = lazy(() => import("./components/Cart/Cart"));
+
 export type EcommerceProps = {};
 
 const Ecommerce = ({}: EcommerceProps) => {
   useEffect(() => {
-    document.getElementById("root").style.background = "#fff";
+    const root = document.getElementById("root") as HTMLElement;
+    root.style.background = "#fff";
   }, []);
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <Navigator />
-      <Panel />
-      <Products />
-    </div>
+    <CartProvider>
+      <div style={{ width: "100%", height: "100%", position: "relative" }}>
+        <Navigator />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Cart />
+        </Suspense>
+        <Panel />
+        <Products />
+      </div>
+    </CartProvider>
   );
 };
 
