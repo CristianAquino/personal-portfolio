718d24313c51a22fb72420ee3281f4b23aa9eb10
cristianaquino <ycristian10@gmail.com>
2023-10-22 23:21:25 -0500

    feat: change component

--- src/pages/Proyects/Ecommerce/components/NavigatorProducts/NavigatorProducts.tsx
+++ src/pages/Proyects/Ecommerce/components/NavigatorProducts/NavigatorProducts.tsx
@@ -1,13 +1,13 @@
-import { Button } from "../Button";
+import { ButtonNavigate } from "../../styles";
 
 export type NavigatorProductsProps = {};
 
 const NavigatorProducts = ({}: NavigatorProductsProps) => {
   return (
     <nav style={{ display: "flex", justifyContent: "center", gap: "16px" }}>
-      <Button>most recent</Button>
-      <Button>lower price</Button>
-      <Button>hightes price</Button>
+      <ButtonNavigate>most recent</ButtonNavigate>
+      <ButtonNavigate>lower price</ButtonNavigate>
+      <ButtonNavigate>hightes price</ButtonNavigate>
     </nav>
   );
 };
