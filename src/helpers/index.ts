2278ed76709175badd308d82f937e64a2b5c008e
cristianaquino <ycristian10@gmail.com>
2023-10-22 23:17:00 -0500

    feat: barrel

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
 export { default as RoutesWithNotFound } from "./RoutesWithNotFound.helper";
 export * from "./emoticons-for-not-found.helper";
 export * from "./unit-decimal.helper";
+export * from "./localStorage.helper";
+export * from "./subjectManagerRxjs.helper";
