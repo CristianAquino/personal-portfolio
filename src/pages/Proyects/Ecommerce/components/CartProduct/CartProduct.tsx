8b93b628e5f228004c1bb149e12efe720313855a
cristianaquino <ycristian10@gmail.com>
2023-10-22 23:20:17 -0500

    styles: cart product style

--- src/pages/Proyects/Ecommerce/components/CartProduct/CartProduct.tsx
+++ src/pages/Proyects/Ecommerce/components/CartProduct/CartProduct.tsx
@@ -1,13 +1,22 @@
+import { BsTrash3 } from "react-icons/bs";
 import { useCustomerContext } from "../../../../../hooks/useCustomerContext";
 import { CartContext } from "../../context/CartContext";
 import { Product } from "../../models";
+import { ButtonRemoveToCart } from "../../styles";
+import {
+  BrandProduct,
+  ButtonCountProduct,
+  NameProduct,
+  PriceProduct,
+} from "../../styles/Product.style";
 
 export type CartProductProps = {
   product: Product;
 };
 
 const CartProduct = ({ product }: CartProductProps) => {
-  const { addToCart, deleteOneToCart } = useCustomerContext(CartContext);
+  const { addToCart, deleteOneToCart, removeFromCart } =
+    useCustomerContext(CartContext);
   return (
     <li
       style={{
@@ -17,90 +26,47 @@ const CartProduct = ({ product }: CartProductProps) => {
         height: "112px",
       }}
     >
-      <div
+      <img
+        src={product.image}
+        alt={product.name}
         style={{
           height: "100%",
+          width: "40%",
           maxWidth: "120px",
+          objectFit: "cover",
+          objectPosition: "center",
           borderRadius: "8px",
-          overflow: "hidden",
         }}
-      >
-        <img
-          src={product.image}
-          alt={product.name}
-          style={{
-            height: "100%",
-            width: "100%",
-            objectFit: "contain",
-            objectPosition: "center center",
-          }}
-        />
-      </div>
+      />
       <div
         style={{
           height: "100%",
-          width: "100%",
+          width: "60%",
           display: "flex",
           flexDirection: "column",
           justifyContent: "space-between",
         }}
       >
-        <div style={{ lineHeight: 1 }}>
-          <p
-            style={{
-              fontWeight: "700",
-              fontSize: "12px",
-              color: "#4C6980",
-              textTransform: "capitalize",
-            }}
-          >
-            {product.brand}
-          </p>
-          <p
-            style={{
-              fontWeight: "bold",
-              fontSize: "16px",
-              color: "#002849",
-              textTransform: "capitalize",
-            }}
-          >
-            {product.name}
-          </p>
-          <p
-            style={{
-              color: "var(--blue)",
-              fontSize: "16px",
-              textTransform: "uppercase",
-              fontWeight: "700",
-            }}
-          >
-            s/. {product.price}
-          </p>
+        <div style={{ position: "relative" }}>
+          <BrandProduct>{product.brand}</BrandProduct>
+          <NameProduct>{product.name}</NameProduct>
+          <PriceProduct>s/. {product.price}</PriceProduct>
+          <ButtonRemoveToCart onClick={() => removeFromCart(product)}>
+            <BsTrash3 />
+          </ButtonRemoveToCart>
         </div>
         <div
           style={{
             width: "50%",
-            border: "1px solid #15DBFF",
+            border: "1px solid var(--primary)",
             borderRadius: "8px",
             display: "flex",
             height: "34px",
           }}
         >
-          <button
-            style={{
-              background: "transparent",
-              fontSize: "1rem",
-              border: "none",
-              cursor: "pointer",
-              color: "#15DBFF",
-              width: "30%",
-              height: "100%",
-              fontWeight: "bold",
-            }}
-            onClick={() => deleteOneToCart(product)}
-          >
+          <ButtonCountProduct onClick={() => deleteOneToCart(product)}>
             -
-          </button>
+          </ButtonCountProduct>
           <span
             style={{
               alignSelf: "center",
@@ -112,21 +78,9 @@ const CartProduct = ({ product }: CartProductProps) => {
           >
             {product.quantity}
           </span>
-          <button
-            style={{
-              background: "transparent",
-              fontSize: "1rem",
-              border: "none",
-              cursor: "pointer",
-              color: "#15DBFF",
-              width: "30%",
-              height: "100%",
-              fontWeight: "bold",
-            }}
-            onClick={() => addToCart(product)}
-          >
+          <ButtonCountProduct onClick={() => addToCart(product)}>
             +
-          </button>
+          </ButtonCountProduct>
         </div>
       </div>
     </li>
