891436e906fc44733654a0644987f4a8e4a36d9d
cristianaquino <ycristian10@gmail.com>
2023-11-07 00:44:34 -0500

    style: change units

--- src/pages/Proyects/pages/Ecommerce/components/CardProduct/styled-component/CardProduct.style.ts
+++ src/pages/Proyects/pages/Ecommerce/components/CardProduct/styled-component/CardProduct.style.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const CardImageProduct = styled.img`
   width: 100%;
-  border-radius: 8px;
-  max-height: 208px;
+  border-radius: 0.5rem;
+  max-height: 13rem;
   object-fit: cover;
   object-position: center;
 `;
