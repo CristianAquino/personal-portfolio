eb55375851328b9ea46eb21380a1321d9a98bd43
cristianaquino <ycristian10@gmail.com>
2023-11-07 00:58:10 -0500

    feat: refactory cartlist component

--- src/pages/Proyects/pages/Ecommerce/components/CartList/CartList.tsx
+++ src/pages/Proyects/pages/Ecommerce/components/CartList/CartList.tsx
@@ -1,135 +1,37 @@
-import { useCallback } from "react";
-import { CartInList } from "..";
 import { useCustomerContext } from "@hooks/index";
+import { CartInList } from "..";
 import { CartContext } from "../../context/CartContext";
-import { checkout } from "../../services";
-import { ButtonNavigate } from "../../style-components";
 import { Main } from "../../style-components/General.style";
+import { OrderSumary } from "./components/OrderSumary";
+import {
+  CartListContent,
+  MessageProducts,
+  ProductsCartListContent,
+} from "./styled-component";
 
 export type CartListProps = {};
 
 const CartList = ({}: CartListProps) => {
   const { cart } = useCustomerContext(CartContext);
-  const checkoutCart = useCallback(async () => {
-    const data = await checkout(cart);
-    window.location.href = data.url;
-  }, []);
+
   return (
     <Main
       style={{
         flexDirection: "column",
       }}
     >
-      <p
-        style={{
-          color: "var(--nameP)",
-          fontSize: "1rem",
-          fontWeight: "bold",
-          textTransform: "capitalize",
-          marginTop: "1rem",
-        }}
-      >
+      <MessageProducts>
         hola estas a un paso de completar tu compra, pero antes te pedimos que
         verifiques que los productos que visualizas sean los que necesitas.
-      </p>
-      {cart.map((product) => (
-        <CartInList cart={product} key={product.id} />
-      ))}
-      <div style={{ color: "black", width: "30%", alignSelf: "flex-end" }}>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          <span
-            style={{
-              fontSize: "16px",
-              fontWeight: "bold",
-              textTransform: "capitalize",
-            }}
-          >
-            subtotal
-          </span>
-          <span
-            style={{
-              fontSize: "16px",
-              textTransform: "capitalize",
-            }}
-          >
-            {" "}
-            s/.{" "}
-            {cart.reduce(
-              (acc, curr) => acc + curr.unitPrice * curr.quantity,
-              0
-            )}
-          </span>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          <span
-            style={{
-              fontSize: "16px",
-              fontWeight: "bold",
-              textTransform: "capitalize",
-            }}
-          >
-            descuento
-          </span>
-          <span
-            style={{
-              fontSize: "16px",
-              textTransform: "capitalize",
-            }}
-          >
-            {" "}
-            s/. {cart.reduce((acc, curr) => acc + curr.unitPrice, 0) * 0.1}
-          </span>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          <span
-            style={{
-              fontSize: "16px",
-              fontWeight: "bold",
-              textTransform: "capitalize",
-            }}
-          >
-            total
-          </span>
-          <span
-            style={{
-              fontSize: "16px",
-              textTransform: "capitalize",
-              fontWeight: "bold",
-            }}
-          >
-            {" "}
-            s/.{" "}
-            {cart.reduce(
-              (acc, curr) => acc + curr.unitPrice * curr.quantity,
-              0
-            )}
-          </span>
-        </div>
-        <ButtonNavigate
-          style={{ width: "100%", marginTop: "16px" }}
-          onClick={checkoutCart}
-        >
-          Pagar
-        </ButtonNavigate>
-      </div>
+      </MessageProducts>
+      <CartListContent>
+        <ProductsCartListContent>
+          {cart.map((product) => (
+            <CartInList product={product} key={product.id} />
+          ))}
+        </ProductsCartListContent>
+        <OrderSumary />
+      </CartListContent>
     </Main>
   );
 };
