5d6f31d46a2e07f5892d29903b807ef25863314c
cristianaquino <ycristian10@gmail.com>
2023-11-09 23:01:54 -0500

    feat: brand component

--- src/pages/Proyects/pages/Ecommerce/components/Brands/Brands.tsx
+++ src/pages/Proyects/pages/Ecommerce/components/Brands/Brands.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { Container, Handle, Slider } from "./styled-component";
 
 export type BrandsProps = {};
 
@@ -32,80 +32,17 @@ const Brands = ({}: BrandsProps) => {
     "LED Lights",
     "luxury palace",
   ];
-  const brandsRef = useRef<HTMLUListElement>(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  function nextSlide() {
-    const sectionBrands = brandsRef.current;
-    const listBrands = sectionBrands?.querySelectorAll("li");
-    const val = currentSlide + 1;
-    setCurrentSlide(val);
-    if (val >= listBrands?.length) {
-      setCurrentSlide(0);
-    }
-    console.log("next-" + val);
-    sectionBrands.style.transform = `translateX(-${
-      val * sectionBrands?.offsetWidth
-    }px)`;
-  }
-
-  function prevSlide() {
-    const sectionBrands = brandsRef.current;
-    const listBrands = sectionBrands?.querySelectorAll("li");
-    const val = currentSlide - 1;
-    setCurrentSlide(val);
-    if (val < 0) {
-      setCurrentSlide(listBrands.length / 3 - 1);
-    }
-    console.log("prev-" + val);
-    sectionBrands.style.transform = `translateX(-${
-      val * sectionBrands?.offsetWidth
-    }px)`;
-  }
 
   return (
-    <section
-      style={{
-        textWrap: "nowrap",
-        overflow: "hidden",
-        alignSelf: "center",
-        position: "relative",
-      }}
-    >
-      <ul
-        style={{ transition: "transform 0.5s ease-in-out", listStyle: "none" }}
-        ref={brandsRef}
-      >
+    <Container>
+      <Handle>&#8249;</Handle>
+      <Slider>
         {brands.map((brand) => (
-          <li
-            key={brand}
-            style={{
-              fontSize: "clamp(1rem, 1rem + 0.7vw, 1.5rem)",
-              margin: "0 0.5rem",
-              background: "#ccc",
-              padding: "0.25rem 0.5rem",
-              display: "inline",
-            }}
-          >
-            {brand}
-          </li>
+          <li key={brand}>{brand}</li>
         ))}
-      </ul>
-      {currentSlide > 0 && (
-        <button
-          style={{ position: "absolute", left: 0, top: 0, bottom: 0 }}
-          onClick={prevSlide}
-        >
-          left
-        </button>
-      )}
-      <button
-        style={{ position: "absolute", right: 0, top: 0, bottom: 0 }}
-        onClick={nextSlide}
-      >
-        right
-      </button>
-    </section>
+      </Slider>
+      <Handle>&#8250;</Handle>
+    </Container>
   );
 };
 
