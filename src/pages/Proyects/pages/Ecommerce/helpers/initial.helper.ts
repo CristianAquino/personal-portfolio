70792769d3222f194d34c939acd1d6e8ec650dd3
cristianaquino <ycristian10@gmail.com>
2023-11-10 00:48:44 -0500

    feat: update

--- src/pages/Proyects/pages/Ecommerce/helpers/initial.helper.ts
+++ src/pages/Proyects/pages/Ecommerce/helpers/initial.helper.ts
@@ -2,8 +2,8 @@ import { CartProviderProps, Product } from "../models";
 
 export const initialCartContext: CartProviderProps<Product> = {
   cart: [],
-  addToCart: (initialProduct: Product) => {},
-  deleteOneToCart: (initialProduct: Product) => {},
-  removeFromCart: (initialProduct: Product) => {},
+  addToCart: () => {},
+  deleteOneToCart: () => {},
+  removeFromCart: () => {},
   clearCart: () => {},
 };
