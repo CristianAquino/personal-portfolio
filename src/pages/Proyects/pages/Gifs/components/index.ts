823edcdb3afe9e60ed2c52fb9ecb8e7eb5254486
cristianaquino <ycristian10@gmail.com>
2023-11-14 21:05:25 -0500

    feat: barrel

--- src/pages/Proyects/pages/Gifs/components/index.ts
+++ src/pages/Proyects/pages/Gifs/components/index.ts
@@ -2,3 +2,5 @@ export * from "./Home";
 export * from "./SingleGif";
 export * from "./ListOfGifs";
 export * from "./SearchOfResults";
+export * from "./TrendingSearches";
+export * from "./Category";
