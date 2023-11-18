d82396504d64e9015ded7c0c3a3f470ede90fc3e
cristianaquino <ycristian10@gmail.com>
2023-11-17 21:16:08 -0500

    feat: update

--- src/pages/Proyects/pages/Gifs/components/SearchForm/SearchForm.tsx
+++ src/pages/Proyects/pages/Gifs/components/SearchForm/SearchForm.tsx
@@ -1,23 +1,25 @@
 import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export type SearchFormProps = {
-  handleSubmit: ({ keyword }: { keyword: string }) => void;
-};
+export type SearchFormProps = {};
 
-const SearchForm = ({ handleSubmit }: SearchFormProps) => {
+const SearchForm = ({}: SearchFormProps) => {
   const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
 
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    handleSubmit({ keyword });
+    navigate(`/proyects/gifs/${keyword}`);
   }
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setKeyword(e.target.value);
   }
+
   return (
     <form onSubmit={onSubmit}>
       <input type="text" value={keyword} onChange={handleChange} />
+      <input type="submit" value="Search" />
     </form>
   );
 };
