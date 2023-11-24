9d8f958d5f25cdef6e4f0234037bdb3d5c6bfb64
cristianaquino <ycristian10@gmail.com>
2023-11-23 19:38:11 -0500

    style: add media query

--- src/pages/Proyects/pages/Gifs/components/ListOfGifs/styled-components/ListOfGifs.styles.ts
+++ src/pages/Proyects/pages/Gifs/components/ListOfGifs/styled-components/ListOfGifs.styles.ts
@@ -6,6 +6,14 @@ const GifsContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-auto-rows: minmax(300px, 500px);
+
+  @media (width<576px) {
+    grid-template-columns: 1fr;
+  }
+
+  @media (576px <=width<920px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export { GifsContainer };
--- src/pages/Proyects/pages/Gifs/components/SingleGif/styled-components/SingleGif.styles.ts
+++ src/pages/Proyects/pages/Gifs/components/SingleGif/styled-components/SingleGif.styles.ts
@@ -1,10 +1,38 @@
 import styled from "styled-components";
 
+const GifTitleImage = styled.figcaption`
+  --minSize: 1rem;
+  --mediumSize: calc(0.5rem + 1vw);
+  --maxSize: 2rem;
+  position: absolute;
+  bottom: 1.25rem;
+  left: 1rem;
+  font-size: clamp(var(--minSize), var(--mediumSize), var(--maxSize));
+  font-weight: bold;
+`;
+
 const GifElement = styled.figure<{ $c: number }>`
   width: 100%;
   max-height: 500px;
-  ${(props) => props.$c % 4 == 0 && "grid-column: 1/-1"};
   position: relative;
+
+  @media (width<576px) {
+    grid-column: 1/-1;
+  }
+  @media (576px <=width<920px) {
+    ${(props) => props.$c % 3 == 0 && "grid-column: 1/-1"};
+  }
+  @media (width>=920px) {
+    ${(props) => props.$c % 4 == 0 && "grid-column: 1/-1"};
+  }
+
+  @media (hover: hover) {
+    &:hover > ${GifTitleImage} {
+      background-color: #03b19f;
+      border-radius: 0.5rem;
+      padding: 0 0.5rem;
+    }
+  }
 `;
 
 const GifImage = styled.img`
@@ -12,14 +40,6 @@ const GifImage = styled.img`
   height: 100%;
   object-fit: cover;
   object-position: center;
-`;
-
-const GifTitleImage = styled.figcaption`
-  position: absolute;
-  bottom: 1.25rem;
-  left: 1rem;
-  font-size: 1.25rem;
-  font-weight: bold;
 `;
 
 export { GifElement, GifImage, GifTitleImage };
