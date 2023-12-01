97c70931e2616309685e028c985f6a12dcbb6c6d
cristianaquino <ycristian10@gmail.com>
2023-11-30 20:28:38 -0500

    feat: index

---
+++ src/pages/Draws/Draws.tsx
@@ -0,0 +1,16 @@
+import { AmongUs, AndroidIcon } from "./components";
+
+export type DrawsProps = {};
+
+const Draws = ({}: DrawsProps) => {
+  return (
+    <div>
+      Draws works!
+      <h1>Android Icon</h1>
+      {/* <AndroidIcon /> */}
+      <AmongUs />
+    </div>
+  );
+};
+
+export default Draws;
