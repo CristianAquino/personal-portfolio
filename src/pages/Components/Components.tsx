574808b6e3dc1b772c573fd7fc744a155c7f79dc
cristianaquino <ycristian10@gmail.com>
2023-12-02 20:31:55 -0500

    feat: home routes

--- src/pages/Components/Components.tsx
+++ src/pages/Components/Components.tsx
@@ -1,6 +1,9 @@
 import { Route } from "react-router-dom";
 import { RoutesWithNotFound } from "@guards/index";
 import { PUBLIC_ROUTE } from "@routes/index";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("./Home/Home"));
 
 export type ComponentsProps = {};
 
@@ -10,8 +13,14 @@ const Components = ({}: ComponentsProps) => {
       message="Component not found"
       pageRedirect={PUBLIC_ROUTE.COMPONENTS}
     >
-      <Route path={PUBLIC_ROUTE.HOME} element={<div>Components</div>} />
-      {/* <Route path={PROYECTS_ROUTE.ECOMMERS} element={<div>Ecommerse</div>} /> */}
+      <Route
+        path={PUBLIC_ROUTE.HOME}
+        element={
+          <Suspense fallback={<h1>Load Home...</h1>}>
+            <Home />
+          </Suspense>
+        }
+      />
     </RoutesWithNotFound>
   );
 };
