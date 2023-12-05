011d7bbd25e1b609932ffb4d93f6564ada5197fc
cristianaquino <ycristian10@gmail.com>
2023-12-04 22:51:49 -0500

    style: update styles of component

--- src/pages/Components/AnimatedFlexCard/styled-components/AnimatedFlexCard.styles.ts
+++ src/pages/Components/AnimatedFlexCard/styled-components/AnimatedFlexCard.styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  block-size: 400px;
+  block-size: 60vmin;
   display: flex;
   flex-wrap: nowrap;
   justify-content: start;
@@ -10,10 +10,10 @@ const Container = styled.div`
 `;
 const Card = styled.label`
   position: relative;
-  inline-size: 80px;
+  inline-size: 10vmin;
   cursor: pointer;
   overflow: hidden;
-  border-radius: 2rem;
+  border-radius: 4vmin;
   display: flex;
   align-items: flex-end;
   transition: 0.6s cubic-bezier(0.28, -0.03, 0, 0.99);
@@ -41,11 +41,11 @@ const Icon = styled.div`
   background-color: #223;
   color: #fff;
   border-radius: 50%;
-  inline-size: 50px;
+  inline-size: 6.25vmin;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 15px;
+  margin: 1.875vmin;
 `;
 const Description = styled.div`
   --fmin: 0.75rem;
@@ -54,10 +54,10 @@ const Description = styled.div`
   display: flex;
   flex-direction: column;
   overflow: hidden;
-  block-size: 80px;
-  inline-size: 520px;
+  block-size: 10vmin;
+  inline-size: 80vmin;
   opacity: 0;
-  translate: 0 30px;
+  translate: 0 3.75vmin;
   transition-delay: 0.3s;
   transition: all 0.3s ease;
 
@@ -71,7 +71,6 @@ const Description = styled.div`
 
   & p {
     color: #b0b0ba;
-    padding-block-start: 5px;
     font-size: clamp(var(--fmin), var(--fmed), var(--fmax));
   }
 `;
@@ -79,7 +78,7 @@ const Description = styled.div`
 const Checkbox = styled.input`
   display: none;
   &:checked + ${Card} {
-    inline-size: 600px;
+    inline-size: 90vmin;
   }
   &:checked + ${Card} ${Description} {
     opacity: 1;
