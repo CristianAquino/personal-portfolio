279c279f83546fb7f68c78c796efebbe01d7b329
cristianaquino <ycristian10@gmail.com>
2024-01-18 00:14:26 -0500

    feat: barrel

--- src/pages/Home/components/index.ts
+++ src/pages/Home/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./Layout";
 export * from "./Principal";
+export * from "./Navigator";
 export * from "./Me";
 export * from "./Projects";
-export * from "./Navigator";
+export * from "./Skills";
