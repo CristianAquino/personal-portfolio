6e11299f58a5d6d2a16984a4a756ce6c0eb34754
cristianaquino <ycristian10@gmail.com>
2024-01-19 16:20:56 -0500

    style: adding responsive

--- src/pages/Home/components/Navigator/styled-components/Navigator.style.ts
+++ src/pages/Home/components/Navigator/styled-components/Navigator.style.ts
@@ -2,12 +2,8 @@ import styled from "styled-components";
 
 const Container = styled.nav`
   position: fixed;
-  inset-inline-start: 2rem;
-  block-size: 100vb;
-  min-width: 4rem;
   display: flex;
   justify-content: center;
-  flex-direction: column;
   gap: 1rem;
   @media (width<=320px) {
     inset-block-end: 1rem;
@@ -16,14 +12,25 @@ const Container = styled.nav`
     inset-inline-start: initial;
     inline-size: 288px;
   }
+  @media (width>320px) {
+    inset-block-end: 1.5rem;
+    flex-direction: row;
+    block-size: initial;
+    inset-inline-start: initial;
+    inline-size: 100vi;
+  }
+  @media (width>=992px) {
+    inset-block-end: initial;
+    flex-direction: column;
+    block-size: 100vb;
+    inset-inline-start: 2rem;
+    inline-size: initial;
+    min-width: 4rem;
+  }
 `;
 const NavItem = styled.div`
-  inline-size: 4rem;
-  block-size: 4rem;
   transition: inline-size 0.5s ease-in-out;
-  border-radius: 1rem;
   overflow-x: hidden;
-  font-size: 2rem;
   background-color: var(--primary);
   color: #fff;
 
@@ -48,18 +55,51 @@ const NavItem = styled.div`
   }
 
   @media (width<=320px) {
-    inline-size: 2rem;
-    block-size: 2rem;
+    inline-size: 2.5rem;
+    block-size: 2.5rem;
     border-radius: 0.5rem;
     font-size: 1rem;
 
     & a span:nth-child(1) {
-      min-inline-size: 2rem;
-      block-size: 2rem;
-      line-height: 2rem;
+      min-inline-size: 2.5rem;
+      block-size: 2.5rem;
+      line-height: 2.5rem;
     }
     & a span:nth-child(2) {
       display: none;
+    }
+  }
+  @media (width>320px) {
+    inline-size: 3rem;
+    block-size: 3rem;
+    border-radius: 0.75rem;
+    font-size: 1.5rem;
+
+    & a span:nth-child(1) {
+      min-inline-size: 3rem;
+      block-size: 3rem;
+      line-height: 3rem;
+    }
+    & a span:nth-child(2) {
+      display: none;
+    }
+  }
+
+  @media (width>=992px) {
+    inline-size: 4rem;
+    block-size: 4rem;
+    border-radius: 1rem;
+    font-size: 2rem;
+
+    & a span:nth-child(1) {
+      min-inline-size: 4rem;
+      block-size: 4rem;
+      line-height: 4rem;
+    }
+    & a span:nth-child(2) {
+      display: block;
+      block-size: 4rem;
+      line-height: 4rem;
     }
   }
 
